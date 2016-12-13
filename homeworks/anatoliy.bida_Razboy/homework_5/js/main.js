79f5a50f43e513cb2f180934274ec74313592f9b
Razboy <anatoliy.bida95@gmail.com>
2016-12-13 16:25:33 +0200

    homework_5

---
+++ homeworks/anatoliy.bida_Razboy/homework_5/js/main.js
@@ -0,0 +1,25 @@
+(function() {
+    var blackThemeButton = document.getElementsByClassName('blcircle')[0];
+    var greyThemeButton = document.getElementsByClassName('grcircle')[0];
+    blackThemeButton.addEventListener('click', function(event) {
+        document.body.style.backgroundColor = "#B3B3B3";
+    });
+    greyThemeButton.addEventListener('click', function(event) {
+        document.body.style.backgroundColor = "#fff";    
+    });
+    
+    var popUp = document.getElementsByClassName('contactme')[0];
+    var popUp = document.getElementsByClassName('contactme')[0];
+    var popUp = document.getElementsByClassName('contactme')[0];
+        document.addEventListener('click', function(event) {
+            document.getElementsByClassName()
+        })
+
+
+
+
+    var copirightYear = document.getElementsByClassName('year')[0];
+    copirightYear.textContent = new Date().getFullYear();
+
+
+})();
